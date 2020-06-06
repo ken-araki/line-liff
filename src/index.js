c2242eb639189ea9dc807f887960c2aede5d1691
ken-araki <k.takagaki.614@gmail.com>
2020-06-07 02:25:38 +0900

    fix tobuy list. (fix style, access api)

--- src/index.js
+++ src/index.js
@@ -1,88 +1,186 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ListGroup from 'react-bootstrap/ListGroup'
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
-function Square(props) {
+class Addition extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.value,
+      onClick: (goods) => props.onClick(goods)
+    };
+  }
+
+  handleChange(e) {
+    this.setState = { value: e.target.value };
+  }
+  handleClick() {
+    const v = this.setState.value;
+    if (v !== '') {
+      this.state.onClick(v);
+    }
+  }
+  render() {
+    return (
+      <InputGroup size="lg">
+        <FormControl
+          placeholder="add tobuy"
+          value={this.state.value}
+          onChange={e => this.handleChange(e)}
+        />
+        <InputGroup.Append>
+          <Button
+            variant="outline-secondary"
+            onClick={() => this.handleClick()}>Add</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    );
+  }
+}
+
+function Item(props) {
+  let variant = props.checked ? "success" : ""
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
+    <ListGroup.Item
+      action
+      onClick={props.onClick}
+      variant={variant}
+    >
+      {props.text}
+    </ListGroup.Item>
   );
 }
 
 class Board extends React.Component {
+  TOBUY_API_URL = process.env.REACT_APP_API_URL + '/api/v1/tobuy';
+  API_TOKEN = process.env.REACT_APP_API_TOKEN;
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
+      tobuyList: [],
+      checked: []
     };
   }
+  componentDidMount() {
+    this.fetchTobuy()
+  }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
+  fetchTobuy() {
+    fetch(this.TOBUY_API_URL, {
+      method: 'GET',
+      headers: new Headers({
+        'token': this.API_TOKEN,
+        'nonce': 'hoge'
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          tobuyList: res.data
+        });
+      })
+  }
+
+  addTobuy(goods) {
+    fetch(`${this.TOBUY_API_URL}/add?goods=${goods}`, {
+      method: 'POST',
+      headers: new Headers({
+        'token': this.API_TOKEN,
+        'nonce': 'hoge'
+      })
+    }).then(_ => {
+      this.fetchTobuy()
     });
   }
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
+
+  buy() {
+    fetch(`${this.TOBUY_API_URL}/buy`, {
+      method: 'POST',
+      body: JSON.stringify(this.state.checked),
+      headers: new Headers({
+        'Content-type': 'application/json',
+        'token': this.API_TOKEN,
+        'nonce': 'hoge'
+      })
+    }).then(_ => {
+      this.setState({ checked: [] })
+      this.fetchTobuy()
+    });
   }
 
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
+  isChecked(i) {
+    return this.state.checked.includes(i)
+  }
+  handleCheck(i) {
+    let checked = this.state.checked;
+    if (this.isChecked(i)) {
+      checked = checked.filter(id => id !== i)
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      checked.push(i)
     }
-
+    this.setState({
+      checked: checked
+    });
+  }
+  handleClickBuy() {
+    const checked = this.state.checked;
+    if (checked.size === 0) {
+      return;
+    }
+    this.buy()
+  }
+  render() {
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <Container fluid="md">
+          <Row className="mt-3">
+            <Col lg="2">
+              <Addition
+                value={this.state.addition}
+                onClick={(goods) => this.addTobuy(goods)}
+              />
+            </Col>
+          </Row>
+          <Row className="mt-3">
+            <Col>
+              <ListGroup>
+                {this.state.tobuyList.map((tobuy) => {
+                  const checked = this.state.checked.includes(tobuy.id)
+                  return <Item
+                    text={tobuy.name}
+                    checked={checked}
+                    onClick={() => this.handleCheck(tobuy.id)}
+                  />;
+                })}
+              </ListGroup>
+            </Col>
+          </Row>
+          <Row className="mt-3">
+            <Col>
+              <Button
+                variant="success"
+                size="lg"
+                block
+                onClick={() => this.handleClickBuy()}>Buy</Button>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
+class Tobuy extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      <div><Board /></div>
     );
   }
   componentDidMount() {
@@ -103,26 +201,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Tobuy />,
   document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
